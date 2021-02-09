37e31049450d9de741929bb4f89abcc3d19eedec
IkaWaAyuMu <pungpond4450@gmail.com>
2021-02-09 15:40:59 +0700

    Commit SHIT.

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&array, int N){
+	array = new char [N+1];    
+	for(int i = 0; i < N;i++) array[i] = 'A'+i;
+	array[N] = 0;
 }
 
 int main(){
